1576ba0a5526606047ab32d76fa9845d98217245
Yunseonghwan <sunghwan0208@naver.com>
2023-02-26 16:01:13 +0900

    fix api 로직

--- web-end-user/src/common/api/api.ts
+++ web-end-user/src/common/api/api.ts
@@ -1,21 +1,73 @@
-import axios from "axios";
+import axios, {
+  AxiosError,
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from "axios";
 
-export const callApi = (
-  method: string,
-  path: string,
-  data?: any,
-  params?: any
-) => {
-  const headers = {
-    "content-type": "application/json",
-  };
-
-  const baseUrl = process.env.REACT_APP_API;
-  const fullUrl = `${baseUrl}${path}`;
-
-  if (method === "get" || method === "delete") {
-    return axios[method](fullUrl, { headers, params });
-  } else if (method === "post" || method === "put") {
-    return axios[method](fullUrl, data, { headers });
-  }
+const onRequest = (config: any): AxiosRequestConfig => {
+  //요청보낼때 headers 객체 추가
+  config.headers["Authorization"] = `Bearer ${localStorage.getItem(
+    "accesstoken"
+  )}`;
+  return config;
 };
+
+const onRequestError = (error: AxiosError): Promise<AxiosError> => {
+  // 요청 error return
+  return Promise.reject(error);
+};
+
+const onResponse = (response: AxiosResponse): AxiosResponse => {
+  // api 응답
+  return response;
+};
+
+const onResponseError = async (error: any): Promise<AxiosError> => {
+  if (error.response) {
+    //액세스토큰 만료 됐을때 들어오는 error 조건 처리
+    if (
+      error.response.status === 401 &&
+      error.response.data.errorMessage === "엑세스 토큰이 만료되었습니다."
+    ) {
+      try {
+        const { data } = await axios.post(``, null, {
+          headers: {
+            "content-type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
+          },
+          params: {
+            RefreshToken: `Bearer ${localStorage.getItem("refreshtoken")}`,
+          },
+        });
+
+        const { accessToken } = data;
+
+        localStorage.setItem("accesstoken", JSON.stringify(accessToken));
+        location.reload();
+      } catch (_error) {
+        if (_error) {
+          localStorage.clear();
+          location.reload();
+        }
+        return Promise.reject(_error);
+      }
+    }
+  }
+  return Promise.reject(error);
+};
+
+const setupInterceptorsTo = (axiosInstance: AxiosInstance): AxiosInstance => {
+  axiosInstance.interceptors.request.use(onRequest, onRequestError);
+  axiosInstance.interceptors.response.use(onResponse, onResponseError);
+  return axiosInstance;
+};
+
+export const api = setupInterceptorsTo(
+  axios.create({
+    baseURL: "",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+);
